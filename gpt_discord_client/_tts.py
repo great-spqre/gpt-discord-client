59d81876dd4177dd043eb17fc0138b34d6217718
great SPQRe <segazhyr@gmail.com>
2024-09-29 13:08:11 +0500

    Update _tts.py

--- gpt_discord_client/_tts.py
+++ gpt_discord_client/_tts.py
@@ -6,7 +6,7 @@ __all__: List[str] = [
     '_create_tts_file'
 ]
 
-
+# etc: you can use silero-tts module if you have a powerful device
 def _create_tts_file(file_name: str, content: str, lang: str = 'en') -> None:
 
     gTTS(text=content, lang=lang).save(file_name)
