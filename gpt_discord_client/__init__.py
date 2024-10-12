3b582107e83c0b7304d0e023e86b826e98ced24e
great SPQRe <segazhyr@gmail.com>
2024-10-12 18:21:34 +0500

    Update __init__.py

--- gpt_discord_client/__init__.py
+++ gpt_discord_client/__init__.py
@@ -18,6 +18,6 @@ from ._gpt_discord_client import *
 from ._discord_listener import *
 from ._listener import *
 from ._tts import *
-from .client import *
+from ._client import *
 
 __version__: str = '0.0.1'
